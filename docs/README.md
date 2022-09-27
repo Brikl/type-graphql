4c76b8e59e11f4485f56a39f10b7fdf315b21f2c
Gusb3ll <Kitpipat.0647@gmail.com>
2022-09-27 15:30:10 +0700

    chore: third impact

--- docs/README.md
+++
@@ -1,4 +0,0 @@
-# Documentation
-
-**The documentation has been moved to [TypeGraphQL website](https://typegraphql.com), please update your bookmarks!**
-https://typegraphql.com/docs/introduction.html
--- examples/middlewares-custom-decorators/decorators/current-user.ts
+++
@@ -1,6 +0,0 @@
-import { createParamDecorator } from "../../../src";
-import { Context } from "../context";
-
-export default function CurrentUser() {
-  return createParamDecorator<Context>(({ context }) => context.currentUser);
-}
